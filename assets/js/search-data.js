89c1c595ead3ace2128c79639cdc8341215104bf
lynspica <92931254+lynspica@users.noreply.github.com>
2025-12-06 02:05:49 +0000

    Deploying to gh-pages from @ lynspica/lynspica.github.io@91dc870746d5c1ca75e480e0d3ef40d7e297245b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,20 +3,13 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
